eba7b67d60ba8e6cc106c49a9ac7cfaf4c0f4c41
ruiange <bombingpig@outlook.com>
2025-05-07 15:42:01 +0800

    feat(i18n): 实现国际化支持并添加多语言功能- 新增 i18n-auto 插件，自动加载 locales目录下的语言文件 - 添加英文和中文的 login 页面语言文件 - 在 nuxt.config.ts 中配置 i18n 模块，设置默认语言和路由策略- 插入国际化插件并配置浏览器语言检测

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
+
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@tdesign-vue-next/nuxt'],
+  modules: ['@tdesign-vue-next/nuxt', '@nuxtjs/i18n'],
   devServer:{
     port: 3007,
     host: '0.0.0.0',
@@ -10,4 +12,25 @@ export default defineNuxtConfig({
   css: [
     '~/assets/css/main.css'
   ],
+  i18n: {
+    // 2. 默认语言
+    defaultLocale: 'zh-CN',
+
+    // 3. 路由前缀方式：
+    //    - 'prefix'：所有语言都带前缀 (/en/about, /zh-CN/about)
+    //    - 'prefix_except_default'：默认语言不带前缀 (/about, /en/about)
+    strategy: 'prefix_except_default',
+
+    // 4. 懒加载语言包
+    lazy: true,
+    langDir: 'locales/',
+
+    // 5. 浏览器语言探测
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root'  // 当访问 / 时根据浏览器语言跳转
+    }
+  },
+  plugins: ['~/plugins/i18n-auto.ts']
 })
--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -31,7 +31,7 @@
     </div>
     <div class="login-left">
       <div class="login-header">
-        <h2 class="main-title">登录到<br />博客管理系统</h2>
+        <h2 class="main-title">登录到<br />博客管理系统 </h2>
         <p class="subtitle">欢迎使用博客后台管理系统</p>
       </div>
       <t-form :data="formData" @submit="handleSubmit" class="login-form" :label-width="0">
