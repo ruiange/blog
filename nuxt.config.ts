21ff6858fa73ff4fecbee815faa93b4448683abf
ruiange <bombingpig@outlook.com>
2025-05-07 17:20:41 +0800

    chore(locales): 将登录页本地化文件从zh-CN重命名为zh

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@tdesign-vue-next/nuxt', '@nuxtjs/i18n'],
+  modules: ['@tdesign-vue-next/nuxt'],
   devServer:{
     port: 3007,
     host: '0.0.0.0',
